038d181f07092c78b444df50c55bfa2d7d6f074c
Natasha Kelly <natashakelly@natashas-mini.myfiosgateway.com>
2021-04-04 16:05:01 -0400

    form validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,5 @@
 class Card < ApplicationRecord
   belongs_to :list
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ApplicationRecord
     has_many :cards
+    validates :name, presence: true
 end
